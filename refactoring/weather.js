26dd6f60b21c531f246bbc307551d8c611257580
Christian Lowe <christglowe@gmail.com>
2018-03-24 20:46:01 -0500

    Refactored project using express.js, solved the fundamental issue that was causing responses to be written to the screen twice

---
+++ refactoring/weather.js
@@ -0,0 +1,48 @@
+const EventEmitter = require("events").EventEmitter;
+//imports the http module
+const http = require('http');
+
+//hides the api key to prevent security leaks
+const key = require('./api.js')
+
+const util = require("util");
+
+
+let weatherID = "10";
+
+
+
+//Main function
+function GetWeather (city = "Houston") {
+let weatherEmitter = new EventEmitter;
+module.exports.weatherEmitter = weatherEmitter;
+  const request = http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key.apikey}`,(response) => {
+            let body = "";
+            let par = "";
+
+            response.on("data", (data) => {
+              body += data;
+              weatherEmitter.emit("data", data);
+            })
+
+            response.on("error", () => {
+                        let message = `Error: $[city] could not be found :(`
+                        console.error(message);
+                        weather.emit('error', error);
+                        return false;
+                        })
+
+            response.on("end", () => {
+               body = body.toString()
+               par = JSON.parse(body)
+               weatherID = par.weather[0].id;
+              module.exports.weatherID = weatherID;
+              weatherEmitter.emit('end');
+              })
+     }) //end request
+
+}; //end function GetWeather
+
+
+
+module.exports.GetWeather = GetWeather;
