edb5d917d1f6e7e57f0af9ba8b7af153f09564a5
Christian Lowe <christglowe@gmail.com>
2018-04-04 11:06:33 -0500

    Made more changes wthin the refactoring file

--- refactoring/server.js
+++ refactoring/server.js
@@ -12,9 +12,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res, next) => {
   res.render('index',{
-    greeting: 'Welcome to Mood.fm',
-    instructions: 'Enter your city below',
-    weatherData: ""
+    greeting: 'Mood.fm',
   } )
   return next();
 })
@@ -25,7 +23,19 @@ app.post("/:city", (req, res, next) => {
   let cityQuery = req.params.city.toString().slice(1);
   weatherRequest.GetWeather(cityQuery);
   weatherRequest.weatherEmitter.on('end', () => {
-    res.render("rain")
+    if (weatherRequest.weatherID >= 300 && weatherRequest.weatherID < 600){
+        res.render("rain")
+    } else if (weatherRequest.weatherID >= 600 && weatherRequest.weatherID < 700) {
+      res.render("snow")
+    } else if (weatherRequest.weatherID >= 800 && weatherRequest.weatherID < 957) {
+      res.render('chillin')
+    } else if (weatherRequest.weatherID >= 200 && weatherRequest.weatherID < 300 || weatherRequest.weatherID >= 957) {
+      res.render('notChill.ejs')
+    } else {
+      res.render("chillin")
+    }
+
+
     console.log("Headers sent?2 ", res.headersSent);
     console.log(count)
     count ++;
